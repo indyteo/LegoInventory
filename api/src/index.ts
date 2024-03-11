6ba2290a0347af5c71cba81018d7d6ab03eecd34
Clopro <cloeberthelin1996@gmail.com>
2024-03-11 14:45:58 +0100

    Fix dockerfile and cors api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { getPort } from "./config";
 import { logAfter, logBefore } from "./handlers/middlewares";
 import { setupBricksHandlers } from "./handlers";
 
 const port = getPort();
 const app = express();
-
+app.use(cors());
 app.use(logBefore);
 
 app.get("/", (_req, res) => {
